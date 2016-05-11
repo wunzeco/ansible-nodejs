5f758a8e7e412e7a435f9af0aa26b28fabad5e83
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-05-11 14:07:45 +0100

    [OI] install build tools for nodejs

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,6 +5,14 @@ describe file('/etc/apt/sources.list.d/nodesource.list') do
   it { should be_mode 644 }
 end
 
+describe package('nodejs') do
+  it { should be_installed }
+end
+
+describe package('build-essential') do
+  it { should be_installed }
+end
+
 describe command("node -v") do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match %r(v6.*) }
